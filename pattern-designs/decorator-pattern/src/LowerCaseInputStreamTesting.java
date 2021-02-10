cf8720f1e47c3d4cd666b34c996a3749a6199c7d
Tam Nguyen <ttamng0905@gmail.com>
2021-02-09 20:59:01 -0700

    Completed the Decorator Pattern

---
+++ pattern-designs/decorator-pattern/src/LowerCaseInputStreamTesting.java
@@ -0,0 +1,32 @@
+import java.io.FilterInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
+public class LowerCaseInputStreamTesting extends FilterInputStream {
+
+    /**
+     * Creates a {@code FilterInputStream}
+     * by assigning the  argument {@code in}
+     * to the field {@code this.in} so as
+     * to remember it for later use.
+     *
+     * @param in the underlying input stream, or {@code null} if
+     *           this instance is to be created without an underlying stream.
+     */
+    protected LowerCaseInputStreamTesting(InputStream in) {
+        super(in);
+    }
+
+    public int read() throws IOException{
+        int c = in.read();
+        return (c == -1 ? c : Character.toLowerCase(c));
+    }
+    public int read(byte[] b, int offset, int len) throws IOException{
+        int result = in.read(b, offset, len);
+        for (int i = offset; i < offset + result; i++){
+            b[i] = (byte)Character.toLowerCase((char) b[i]);
+        }
+        return result;
+    }
+
+}
