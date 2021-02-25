3d4b5c1c1654a1f9ea10f1f94de7a64914bdaa7d
Tam Nguyen <ttamng0905@gmail.com>
2021-02-24 23:43:20 -0700

    Command Pattern

---
+++ pattern-designs/command-pattern/src/Light.java
@@ -0,0 +1,14 @@
+public class Light {
+    public boolean isOn;
+    public Light(){
+
+    }
+
+    public void on(){
+        this.isOn = true;
+    }
+
+    public void off(){
+        this.isOn = false;
+    }
+}
