61b9a266df4f0be1589d10f6b39637bfeae02875
Tam Nguyen <ttamng0905@gmail.com>
2021-02-27 14:23:38 -0700

    initial commit for java web

---
+++ java-web/src/main/java/book/java/java_web/HelloApplication.java
@@ -0,0 +1,9 @@
+package book.java.java_web;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
