43b5469bfab649873cf3f5307c6314ab46251310
Tam Nguyen <ttamng0905@gmail.com>
2021-02-26 23:28:14 -0700

    drafting numpy

---
+++ python-data/main.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+data = np.random.randn(2, 3, 2)
+
+print(data)
+
+print(data * 10)
+
+arr1 = np.array([1, 2, 3], dtype=np.float64)
+
+arr = np.arange(10)
+
+arr
+
+arr[5:8] = 12
+
+print(arr)
