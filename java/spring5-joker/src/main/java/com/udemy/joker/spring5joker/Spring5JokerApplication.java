dffc45c734127700bb676b87cb62c072e219e86f
Tam Nguyen <ttamng0905@gmail.com>
2021-03-06 14:43:08 -0700

    initial commit

---
+++ java/spring5-joker/src/main/java/com/udemy/joker/spring5joker/Spring5JokerApplication.java
@@ -0,0 +1,13 @@
+package com.udemy.joker.spring5joker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5JokerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring5JokerApplication.class, args);
+    }
+
+}
---
+++ java/spring5-joker/src/test/java/com/udemy/joker/spring5joker/Spring5JokerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udemy.joker.spring5joker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5JokerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
