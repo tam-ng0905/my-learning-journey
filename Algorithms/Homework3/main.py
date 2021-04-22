73f1f455885e916385a70712a6ebe89f640584e5
Tam Nguyen <tam.t.nguyen@du.edu>
2021-04-22 15:15:59 -0600

    added homework3

---
+++ Algorithms/Homework3/main.py
@@ -0,0 +1,15 @@
+def swap(a, b):
+	temp = a
+	a = b
+	b = temp
+	return a,b
+
+def partition(A, low, high):
+	pivot = A[len(A) - 1]
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print(swap(5,8))
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
