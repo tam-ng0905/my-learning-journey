5deb97bdc369a9eeb304998384345fe22add44a0
Tam Nguyen <tam.t.nguyen@du.edu>
2021-05-01 17:51:19 -0600

    update mechanic learning

--- pytorch/basic-pytorch/mechanic-learning.py
+++ pytorch/basic-pytorch/mechanic-learning.py
@@ -1,10 +1,47 @@
 import torch
 import matplotlib.pyplot as plt
 
-
-t_c = [0.5, 14.0, 15.0, 28.0, 11.0, -4.0, 13.0]
-t_u = [35.7, 55.9, 58.2, 81.9, 56.3, 48.9, 33.9]
+t_c = [0.5, 14.0, 15.0, 28.0, 11.0, 8.0, 3.0, -4.0, 6.0, 13.0, 21.0]
+t_u = [35.7, 55.9, 58.2, 81.9, 56.3, 48.9, 33.9, 21.8, 48.4, 60.4, 68.4]
 t_c = torch.tensor(t_c)
 t_u = torch.tensor(t_u)
 
-plt.plot(t_c)
+plt.plot(t_c, t_u)
+plt.ylabel('Celcius')
+plt.xlabel('Measurement')
+plt.show()
+
+plt.plot(t_u, t_c, 'o', color='black')
+plt.show()
+
+
+def model(t_u, w, b):
+    return w * t_u + b
+
+
+def loss_fn(t_p, t_c):
+    squared_diffs = (t_p - t_c) ** 2
+    return squared_diffs.mean()
+
+
+w = torch.ones(())
+b = torch.zeros(())
+
+t_p = model(t_u, w, b)
+print(t_p)
+
+loss = loss_fn(t_p, t_c)
+print(loss)
+learning_rate = 1e-2
+delta = 0.1
+loss_rate_of_change_w = \
+    (loss_fn(model(t_u, w + delta, b), t_c) -
+     loss_fn(model(t_u, w - delta, b), t_c)) / (2.0 * delta)
+
+w = w - learning_rate * loss_rate_of_change_w
+
+loss_rate_of_change_b = \
+    (loss_fn(model(t_u, w, b + delta), t_c) -
+     loss_fn(model(t_u, w, b - delta), t_c)) / (2.0 * delta)
+
+b = b - learning_rate * loss_rate_of_change_b
