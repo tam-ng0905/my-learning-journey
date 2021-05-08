d5f43cd246b4e2e4b1b1c1b81f27fe47c058574b
Tam Nguyen <tam.t.nguyen@du.edu>
2021-05-08 12:37:09 -0600

    more go

--- Go/first/start.go
+++ Go/first/start.go
@@ -2,6 +2,14 @@ package main
 
 import "fmt"
 
-func main(){
+var flag bool;
+func main() {
 	fmt.Println("Hello, world !")
+	first := false
+	flag = true
+	fmt.Println(flag)
+	fmt.Println(first)
+
+	var x int = 10
+	x *= 2
 }
