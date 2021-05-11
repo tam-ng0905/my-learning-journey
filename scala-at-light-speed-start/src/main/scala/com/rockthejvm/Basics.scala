78439cc884c9695801645713279c12a11fef923d
Tam Nguyen <tam.t.nguyen@du.edu>
2021-05-11 13:49:51 -0600

    basics

--- scala-at-light-speed-start/src/main/scala/com/rockthejvm/Basics.scala
+++ scala-at-light-speed-start/src/main/scala/com/rockthejvm/Basics.scala
@@ -1,5 +1,15 @@
 package com.rockthejvm
 
-class Basics {
+class Basics extends App{
+  val meaningOfLie: Int = 42
+
+  val aBoolean = false //type is optional
+
+  val aString = "I love Scala"
+
+  val composeString = "I " + "love" + aString
+
+  val aInterpolation = s"The meaning of life is $meaningOfLie"
+
 
 }
