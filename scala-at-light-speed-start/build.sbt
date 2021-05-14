7352895f242f4dbbd64584577388729a1465deb1
Tam Nguyen <tam.t.nguyen@du.edu>
2021-05-14 10:04:42 -0600

    errors with akka

--- scala-at-light-speed-start/build.sbt
+++ scala-at-light-speed-start/build.sbt
@@ -3,3 +3,10 @@ name := "scala-at-light-speed"
 version := "0.1"
 
 scalaVersion := "2.13.1"
+
+val akkaVersion = "2.5.13"
+libraryDependencies ++= Seq(
+  "com.typesafe.akka" %% "akka-actor" % akkaVersion,
+  "com.typesafe.akka" %% "akka-testkit" % akkaVersion,
+  "org.scalatest" %% "scalatest" % "3.0.5"
+)
--- scala-at-light-speed-start/src/main/scala/akka/ActorIntro.scala
+++ scala-at-light-speed-start/src/main/scala/akka/ActorIntro.scala
@@ -1,7 +1,27 @@
 package akka
+import akka.actor.{ActorSystem, Actor, Props}
 
 class ActorIntro extends App{
   //part 1 - actor systems
   val actorSystem = ActorSystem("firstActorSystem")
-  println(actorSystem)
+  println(actorSystem.name)
+
+  //part 2 - create actors
+  //actors are uniquely identified within a system
+  //each actor may respond differently
+  //actor are encapsulated
+
+  class WordCountActor extends Actor{
+    var totalWords = 0
+    def receive: PartialFunction[Any, Unit] = {
+      case message: String => totalWords += message.split(" ").length
+      case msg => println(s"[Word counter] I cannot understand ${msg.toString}")
+    }
+  }
+
+  //part3 - instantiate our actor
+  val wordCounter = actorSystem.actorOf(Props[WordCountActor], "wordCounter")
+
+
+
 }
