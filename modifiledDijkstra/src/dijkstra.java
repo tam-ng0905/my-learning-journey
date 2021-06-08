8b03b342c5232d7dac0f264221161da47247c58c
Tam Nguyen <tam.t.nguyen@du.edu>
2021-06-08 03:14:06 -0600

    done with the adjacent list

--- modifiledDijkstra/src/dijkstra.java
+++ modifiledDijkstra/src/dijkstra.java
@@ -4,7 +4,10 @@ import java.io.FileNotFoundException;
 import java.util.*;
 
 public class dijkstra {
-    private PriorityQueue<Node> queue;
+    private static PriorityQueue<Node> queue = new PriorityQueue<Node>();
+    private static List<List<Node>> list = new LinkedList<>();
+    private int distance[];
+
     static class Node{
         private Character key;
         private float value;
@@ -17,50 +20,52 @@ public class dijkstra {
             return key + " " + value;
         }
     }
-    static class Graph{
-//        private int vertices;
-        private LinkedList<Node> list;
 
-        Graph() {
-//            this.vertices = vertices;
-            this.list = new LinkedList<Node>();
-        }
-        void addVertice(Node v){
-            this.list.add(v);
-        }
-        public String toString(){
-            return list.toString();
-        }
-
-    }
     public static void main(String[] args) throws FileNotFoundException {
         File text = new File("src/text.txt");
         Scanner input = new Scanner(text);
-        List<Graph> lines = new ArrayList<Graph>();
         try {
 
             while (input.hasNextLine()) {
                 String line = input.nextLine();
                 char[] ln = line.toCharArray();
                 char first = ln[0];
-                Graph adList = new Graph();
+                List<Node> adList = new LinkedList<>();
 
-                adList.addVertice(new Node(first, 0));
+                adList.add(new Node(first, 0));
                 for(int i = 1; i < ln.length; i++) {
                     if(!Character.isDigit(ln[i]) && Character.isDigit(ln[i+1])){
-                        adList.addVertice(new Node(ln[i], (float) (ln[i + 1])));
+                        adList.add(new Node(ln[i], (float) (ln[i + 1])));
                     }
                 }
-                lines.add(adList);
+                list.add(adList);
             }
 
-            System.out.println(lines);
+
         } finally{
             input.close();
         }
         input.close();
+        Set<Node> hash_Set = new HashSet<Node>();
+        float[] distance = new float[list.size()];
+        queue.add(list.get(0).get(0));
+        while(queue.size() > 0){
+            Node current = queue.peek();
+            int index = 0;
+            for(int i = 0; i < list.size(); i++){
+                if(list.get(i).get(0).equals(current)){
+                    index = i;
+                }
+            }
+            hash_Set.add(current);
+            for(Node a : list.get(index)){
+                if((distance[index] + a.value) < distance[index+1]){
+
+                }
+            }
+        }
 
 
-        queue = new PriorityQueue<Node>(V, new Node());
+
     }
 }
