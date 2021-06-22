4b47ba6e5b72804d0769c20142485533e4dc42f8
Tam Nguyen <tam.t.nguyen@du.edu>
2021-06-22 15:41:27 -0600

    added db

--- fastAPI/blog/main.py
+++ fastAPI/blog/main.py
@@ -1,12 +1,23 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from . import schema, models
-from database import engine
-
+from .database import engine, SessionLocal
+from sqlalchemy.orm import Session
 models.Base.metadata.create_all(engine)
 
 app = FastAPI()
 
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
 
 @app.post('/blog')
-def create(request: schema.Blog):
-    return request
+def create(request: schema.Blog, db: Session = Depends(get_db)):
+    new_blog = models.Blog(title=request.title, body=request.body)
+    db.add(new_blog)
+    db.commit()
+    db.refresh(new_blog)
+    return new_blog
--- fastAPI/blog/models.py
+++ fastAPI/blog/models.py
@@ -1,7 +1,10 @@
 from sqlalchemy import Column, Integer, String
-from database import Base
+from .database import Base
+
 
 class Blog(Base):
+    __tablename__ = 'blogs'
+
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String)
     body = Column(String)
