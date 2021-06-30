98855769c2c5116b84fc2bd46dc09a31a8d5fe86
Tam Nguyen <tam.t.nguyen@du.edu>
2021-06-30 12:07:18 -0600

    added models

--- djangoU/djangoU/settings.py
+++ djangoU/djangoU/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'challenges'
+    'challenges',
+    'book_outlet'
 ]
 
 MIDDLEWARE = [
