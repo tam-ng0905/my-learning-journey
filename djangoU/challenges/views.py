94bf1c1c3078765392ebe84eced558eaeaa9ab57
Tam Nguyen <tam.t.nguyen@du.edu>
2021-06-30 10:48:18 -0600

    more on templates

--- djangoU/challenges/views.py
+++ djangoU/challenges/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 from django.urls import reverse
+from django.template.loader import render_to_string
 monthly_challenges = {
     "january": "eat no meat",
     "february": "walk for at least 20 minutes every day!",
@@ -9,19 +10,22 @@ monthly_challenges = {
     "may": "eat no meat",
     "june": "walk for at least 20 minutes every day!",
     "july": "learn django for at least 20 minutes everyday",
-    "august": "eat no meat",
+    "august": None,
 }
 
 def index(request):
     list_items = ""
     months = list(monthly_challenges.keys())
 
-    for month in months:
-        capitalized_month = month.capitalize()
-        month_path = reverse("month-challenge", args=[month])
-        list_items += f"<li><a href=\"{month_path}\">{capitalized_month}</a></li>"
-    response_data = f"<ul>{list_items}</ul>"
-    return HttpResponse(response_data)
+    return render(request, "challenges/index.html", {
+        "months": months
+    })
+    # for month in months:
+    #     capitalized_month = month.capitalize()
+    #     month_path = reverse("month-challenge", args=[month])
+    #     list_items += f"<li><a href=\"{month_path}\">{capitalized_month}</a></li>"
+    # response_data = f"<ul>{list_items}</ul>"
+    # return HttpResponse(response_data)
 # Create your views here.
 def january(request):
     return HttpResponse("Eat no meat for the entire month!")
@@ -32,14 +36,19 @@ def march(request):
     return HttpResponse("Learn Django")
 
 def monthly_challenge(request, month):
-    challenge_text = None
+    challenge_text = monthly_challenges[month]
     # if month == 'january':
     #     challenge_text = "Eat no meat for entire month"
     # elif month == 'february':
     #     challenge_text = "Walk 20 minutes"
     try:
-        response_data = f"<h1>{challenge_text}</h1>"
-        challenge_text = monthly_challenges[month]
-        return HttpResponse(response_data)
+        # response_data = f"<h1>{challenge_text}</h1>"
+        # response_data = render_to_string("challenges/challenge.html")
+        # challenge_text = monthly_challenges[month]
+        # return HttpResponse(response_data)
+        return render(request, 'challenges/challenge.html', {
+            "text": challenge_text,
+            "month_name": month.capitalize()
+        })
     except:
         return HttpResponseNotFound("<h1>This month is not supported!</h1>")
--- djangoU/djangoU/settings.py
+++ djangoU/djangoU/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'challenges'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,9 @@ ROOT_URLCONF = 'djangoU.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
+        'DIRS': [
+        #    BASE_DIR / 'challenges' / 'templates'
+        ]
         ,
         'APP_DIRS': True,
         'OPTIONS': {
