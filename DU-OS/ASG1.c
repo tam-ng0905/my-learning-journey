42bd461981059aff150316eb2ac4e2b49cfcff6c
Tam Nguyen <tam.t.nguyen@du.edu>
2021-09-23 23:13:45 -0600

    more on homework 1

--- DU-OS/ASG1.c
+++ DU-OS/ASG1.c
@@ -1,11 +1,21 @@
 #include <stdio.h>
-//int parse_command(char *inp, int * argc, char * argv[]){
-//    for(int i = 0; &inp[i] != "\0"; i++ ){
-////        printf("%c\n", inp[i]);
-//    }
-//
-//    return *argc;
-//};
+int parse_command(char *inp, int *argc){
+    int i = 0;
+    printf("%d\n", argc);
+    while(inp[i]){
+        if(i == 0){
+            ++*argc;
+        }
+        if(inp[i] == ' ') {
+            ++*argc;
+            printf("%d\n", *argc);
+        }
+        printf("%c\n", inp[i]);
+        i++;
+    }
+
+    return 0;
+};
 
 
 
@@ -21,9 +31,9 @@ int main() {
 //    printf("%d", b);
 //    printf("a - %d", &a);
 
-    char * test = "Hello World Hello Again";
-    char c = 'Z';
-    char a[] = "Hello World";
+//    char * test = "Hello World Hello Again";
+//    char c = 'Z';
+//    char a[] = "Hello World";
 
 //    printf("%c\n", &c);
 //
@@ -32,12 +42,16 @@ int main() {
 //    printf("%c\n", *ptr1);
 //    printf("%c\n", *ptr2);
 
-    printf("%d\n", *test);
-    printf("%lu\n", (sizeof(test)/sizeof(*test)));
+    char test[] = "Hello World";
+    char * first = &test[0];
+//    printf("%lu\n", sizeof(test));
+//    printf("%lu\n", sizeof(first));
+    int arg = 0;
+    int * argc = &arg;
+    parse_command(first, argc);
 
-
-    for(int i = 0; i < *test; i++ ){
-        printf("%c", test[i]);
-    }
+//    for(int i = 0; i < *test; i++ ){
+//        printf("%c", test[i]);
+//    }
     return 0;
 }
