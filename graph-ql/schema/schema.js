c1b093d1cd6b90138db5ae790ff42376801d4b7f
Tam Nguyen <45498088+tam-ng0905@users.noreply.github.com>
2022-03-31 23:33:35 -0600

    initial commit

---
+++ graph-ql/schema/schema.js
@@ -0,0 +1,24 @@
+const {GraphQLString, GraphQLObjectType} = require("graphql");
+
+const BookType = new GraphQLObjectType({
+    name: "Book",
+    fields: () => ({
+        id: {type: GraphQLString},
+        name: {type: GraphQLString},
+        genre: {type: GraphQLString},
+    })
+})
+
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: {
+        book: {
+            type: BookType,
+            args: {id: {type: GraphQLString}},
+            resolve(parent, args){
+                //code to get data from db/other source
+            }
+        }
+    }
+})
+
